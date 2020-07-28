6d11febd83b1b65b604db83c32e6f7f3132d6628
Rishi Advani <rishi1999@gmail.com>
2020-07-28 17:02:51 -0400

    Remove unused code from rputil.py; add docstrings

--- presentations/util/rputil.py
+++ presentations/util/rputil.py
@@ -1,77 +1,40 @@
 import numpy as np
 from numpy import random
-import math
 from matplotlib import pyplot as plt
 import scipy.linalg
 
-def random_svd_rank_k(A, k, power=1, full_m = True):
-    omega = random.randn(A.shape[1],k)
+def svd_rank_k(A, k,full_m=True):
+    """Calculates a deterministic rank-k approximation of a singular value decomposition of the input matrix."""
+    U, sigma, Vh = np.linalg.svd(A, full_matrices=full_m)
+    Sigma = np.diag(sigma[:k])
+    return U[:,:k] @ Sigma @ Vh[:k]
+
+def random_svd_rank_k(A, k, power=1, full_m=True):
+    """Calculates a randomized rank-k approximation of a singular value decomposition of the input matrix."""
+    omega = random.randn(A.shape[1], k)
+    Y = A @ omega
     if power != 0:
-        Y = np.linalg.matrix_power(A @ A.T, power) @ (A @ omega)
-    else:
-        Y = A @ omega
-    #Y = np.linalg.matrix_power(A @ A.T, power) @ (A @ omega)
+        Y = np.linalg.matrix_power(A @ A.T, power) @ Y
     Q, R = np.linalg.qr(Y)
     B = Q.T @ A
-    U_tilde, Sigma, Vh = np.linalg.svd(B, full_matrices = full_m)
+    U_tilde, sigma, Vh = np.linalg.svd(B, full_matrices=full_m)
     U = Q @ U_tilde
-    Sigma = np.diag(Sigma)
+    Sigma = np.diag(sigma)
     return U @ Sigma @ Vh[:k]
 
-def svd_rank_k(matrix, k,full_m = True ):
-    u, sigma, vh = np.linalg.svd(matrix, full_matrices = full_m) #compute full svd
-    u = u[:,:k] #keep all rows, take first k columns
-    sigma = np.diag(sigma[:k]) #take first k singular values and make diagonal matrix
-    vh = vh[:k] #take first k rows, take all columns
-    return u @ sigma @ vh #return rank k approximation
+def id_rank_k(A, k, full_m='full'):
+    """Calculates a deterministic rank-k approximation of an interpolative decomposition of the input matrix."""
+    Q, R, P = scipy.linalg.qr(A, pivoting=True, mode=full_m)
+    Q = Q[:,:k]
+    return Q @ Q.T @ A
 
-def id_rank_k(matrix, k, full_m = 'full'):
-    q, r, p = scipy.linalg.qr(matrix, pivoting=True, mode = full_m)
-    q = q[:,:k]
-    return q @ q.T @ matrix
-
-def random_id_rank_k(matrix, k, oversampling=10, full_m = 'full'):
+def random_id_rank_k(A, k, oversampling=10, full_m='full'):
+    """Calculates a randomized rank-k approximation of an interpolative decomposition of the input matrix."""
     p = k + oversampling
-    if(p<=k or p>matrix.shape[1]):
-        print('Invalid p')
-        return False
-    cols = np.random.choice(matrix.shape[1], replace = False, size = p)
-    AS = matrix[:,cols]
-    q,r,p = scipy.linalg.qr(AS,pivoting=True, mode = full_m)
-    q = q[:,:k]
-    return q @ q.T @ matrix
-
-def proj(v,u): #projection of v onto u
-    return (np.dot(v,u) / np.dot(u,u)) * u
-
-def id_rank_k_slow(matrix, k):
-    
-    m = np.copy(matrix) #starts out as full matrix
-    indices = []
-        
-    if False:
-        indices = predetermined[:k]
-    else:
-        #Greedy determinant maximization process
-        for j in range(k):
-            col_norms = np.zeros(matrix.shape[1])
-            for ind in np.delete(np.arange(matrix.shape[1]), indices): #loop through non-chosen columns
-                col_norms[ind] = np.linalg.norm(m[:,ind]) #gets norms of non-chosen columns
-            max_norm_index = np.argmax(col_norms) #index of column with greatest l2 norm
-
-            indices.append(max_norm_index) #add the index to a list of indices
-
-            #orthogonalize columns with all other indices
-            for i in np.delete(np.arange(matrix.shape[1]), indices):   #iterate over all other indices and do g-s step
-                for ind in indices:
-                    m[:,i] = m[:,i] - proj(m[:,i],m[:,ind])
-
-    Ak = matrix[:,indices] # first k columns of A
-    Other = matrix[:,np.delete(np.arange(matrix.shape[1]),indices)] # other columns of A
-    AP = np.concatenate((Ak,Other),axis=1) # combine it so we have the data, A, reordered, with the 'best' k coming first
-
-    q,r = np.linalg.qr(AP) #now compute QR
-    q = q[:,:k] # and take the first k, corresponding to the 'best' k from A
-
-    return q @ q.T @ matrix
-    
+    if(p <= k or p > A.shape[1]):
+        raise ValueError('Invalid p')
+    cols = np.random.choice(A.shape[1], replace=False, size=p)
+    AS = A[:,cols]
+    Q, R, P = scipy.linalg.qr(AS, pivoting=True, mode=full_m)
+    Q = Q[:,:k]
+    return Q @ Q.T @ A
